c05173a824beee8412759d52999632a3e948df36
Blitheness <blitheness@oneiric.co.uk>
2018-11-29 21:14:03 +0000

    Laravel 5.7 with Socialite

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use Socialite;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('twitter')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $user         = Socialite::driver('twitter')->user();
+
+        $token        = $user->token;
+        $refreshToken = $user->refreshToken;
+        $expiresIn    = $user->expiresIn;
+        $userId       = $user->getId();
+        $knownAs      = $user->getNickname();
+        $userName     = $user->getName();
+        $userEmail    = $user->getEmail();
+        $userAvatar   = $user->getAvatar();
+    }
+}
